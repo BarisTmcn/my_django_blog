5a90a3892be2ba4224a2f35f2852a542625bad1e
Baris Tamcan <tamcanbaris@gmail.com>
2019-05-27 11:02:15 +0300

    QuerySet lookup modified

--- src/blog/views.py
+++ src/blog/views.py
@@ -8,8 +8,10 @@ from .models import BlogPost
 
 
 def blog_post_detail_page(request, slug):
-	#obj = BlogPost.objects.get(id=post_id)
-	obj = get_object_or_404(BlogPost, slug=slug)
+	queryset = BlogPost.objects.filter(slug=slug)
+	if queryset.count() >= 1:
+		obj = queryset.first()
+	#obj = get_object_or_404(BlogPost, slug=slug)
 	template_name = 'blog_post_detail.html'
 	context = {"object": obj}
 	return render(request, template_name, context)
