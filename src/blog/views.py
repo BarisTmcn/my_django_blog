b0ac719855bf326fef09fcae077227626a512bb4
Baris Tamcan <tamcanbaris@gmail.com>
2019-05-28 11:19:07 +0300

    CRUD view outlines added & modified

--- src/blog/views.py
+++ src/blog/views.py
@@ -16,3 +16,39 @@ def blog_post_detail_page(request, slug):
 	template_name = 'blog_post_detail.html'
 	context = {"object": obj}
 	return render(request, template_name, context)
+
+#CRUD (create, retrieve, update, delete)
+
+#GET --> retireve/list
+#POST --> create/update/delete
+
+def blog_post_list_view(request):
+	#list out objects,maybe search
+	template_name = "blog_post_list.html"
+	context = {"objetc_list": []}
+	return render(request, template_name, context)
+
+def blog_post_create_view(request):
+	#create objects using a form
+	template_name = "blog_post_create.html"
+	context = {"form": None}
+	return
+
+def blog_post_detail_view(request, slug):
+	# 1 object-->detail view
+	obj = get_object_or_404(BlogPost, slug=slug)
+	template_name = "blog_post_detail.html"
+	context = {"object": obj}
+	return
+
+def blog_post_update_view(request, slug):
+	obj = get_object_or_404(BlogPost, slug=slug)
+	template_name = "blog_post_update.html"
+	context = {"object": obj, "form": None}
+	return
+
+def blog_post_delete_view(request, slug):
+	obj = get_object_or_404(BlogPost, slug=slug)
+	template_name = "blog_post_delete.html"
+	context = {"object": obj}
+	return
