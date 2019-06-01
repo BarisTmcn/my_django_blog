005f8325009861b2276476a181f5c1c0eb099c97
Baris Tamcan <tamcanbaris@gmail.com>
2019-06-01 21:49:16 +0300

    Model forms and data validation on fields added

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,6 +1,28 @@
 from django import forms
+from .models import BlogPost
+
 
 class BlogPostForm(forms.Form):
 	title = forms.CharField()
 	slug = forms.SlugField()
 	content = forms.CharField(widget=forms.Textarea)
+
+
+class BlogPostModelForm(forms.ModelForm):
+	class Meta:
+		model = BlogPost
+		fields = ['title', 'slug', 'content']
+
+	def clean_email(self, *args, **kwargs):
+		email = self.cleaned_data.get('email')
+		print(email)
+		if email.endswith(".edu"):
+			raise forms.ValidationError("This is not a valid email.Please don't use  .edu")
+		return email
+
+	def clean_title(self, *args, **kwargs):
+		title = self.cleaned_data.get('title')
+		qs = BlogPost.objects.filter(title=title)
+		if qs.exists():
+			raise forms.ValidationError("This title already been used. Please try again.")
+		return title	
